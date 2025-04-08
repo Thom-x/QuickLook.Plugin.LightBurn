4f535da9b83869f22bdf6105886decaaac727cb5
Thom-x <maugin.thomas@gmail.com>
2025-04-08 14:51:28 +0200

    feat(lightburn): add preview of lightburn projects

--- Plugin.cs
+++ Plugin.cs
@@ -1,9 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Xml.Linq;
+using System.Xml.XPath;
 using QuickLook.Common.Plugin;
 
-namespace QuickLook.Plugin.HelloWorld
+namespace QuickLook.Plugin.LightBurn
 {
     public class Plugin : IViewer
     {
@@ -15,7 +19,7 @@ namespace QuickLook.Plugin.HelloWorld
 
         public bool CanHandle(string path)
         {
-            return !Directory.Exists(path) && path.ToLower().EndsWith(".zzz");
+            return !Directory.Exists(path) && path.ToLower().EndsWith(".lbrn2");
         }
 
         public void Prepare(string path, ContextObject context)
@@ -25,12 +29,50 @@ namespace QuickLook.Plugin.HelloWorld
 
         public void View(string path, ContextObject context)
         {
-            var viewer = new Label {Content = "I am a Label. I do nothing at all."};
+            try
+            {
+                var xml = XDocument.Load(path);
+                var thumbnailBase64 = xml.XPathSelectElement("LightBurnProject/Thumbnail")?.Attribute("Source").Value;
+                if (!string.IsNullOrEmpty(thumbnailBase64))
+                {
+                    try
+                    {
+                        var imageBytes = Convert.FromBase64String(thumbnailBase64);
+                        using (var stream = new MemoryStream(imageBytes))
+                        {
+                            var bitmap = new BitmapImage();
+                            bitmap.BeginInit();
+                            bitmap.StreamSource = stream;
+                            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                            bitmap.EndInit();
 
-            context.ViewerContent = viewer;
-            context.Title = $"{Path.GetFileName(path)}";
-
-            context.IsBusy = false;
+                            var image = new Image { Source = bitmap };
+                            context.ViewerContent = image;
+                            context.Title = $"{Path.GetFileName(path)}";
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        context.ViewerContent = new Label { Content = "Error: Thumbnail data is not a valid Base64 string." };
+                    }
+                    catch (Exception ex)
+                    {
+                        context.ViewerContent = new Label { Content = $"Error rendering thumbnail: {ex.Message}" };
+                    }
+                }
+                else
+                {
+                    context.ViewerContent = new Label { Content = $"Error: No thumbnail data found in the file. {path}" };
+                }
+            }
+            catch (Exception ex)
+            {
+                context.ViewerContent = new Label { Content = $"Error loading file: {ex.Message}" };
+            }
+            finally
+            {
+                context.IsBusy = false;
+            }
         }
 
         public void Cleanup()
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ![QuickLook icon](https://user-images.githubusercontent.com/1687847/29485863-8cd61b7c-84e2-11e7-97d5-eacc2ba10d28.png)
 
-# QuickLook.Plugin.HelloWorld
+# QuickLook.Plugin.LightBurn
 
 This plugin illustrates the development process of [QuickLook](https://github.com/QL-Win/QuickLook).
 
 ## Try out
 
-1. Go to [Release page](https://github.com/QL-Win/QuickLook.Plugin.HelloWorld/releases) and download the latest version.
+1. Go to [Release page](https://github.com/QL-Win/QuickLook.Plugin.LightBurn/releases) and download the latest version.
 2. Make sure that you have QuickLook running in the background. Go to your Download folder, and press <key>Spacebar</key> on the downloaded `.qlplugin` file.
 3. Click the “Install” button in the popup window.
 4. Restart QuickLook.
@@ -18,7 +18,7 @@ This plugin illustrates the development process of [QuickLook](https://github.co
  1. Clone this project. Do not forget to update submodules.
  2. Build project with `Release` profile.
  3. Run `Scripts\pack-zip.ps1`.
- 4. You should find a file named `QuickLook.Plugin.HelloWorld.qlplugin` in the project directory.
+ 4. You should find a file named `QuickLook.Plugin.LightBurn.qlplugin` in the project directory.
 
 ## License
 
